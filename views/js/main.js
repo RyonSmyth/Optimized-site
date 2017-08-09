7a0de96e29b6a26e84c9b28e9b5f8fac67b470b6
RyonSmyth <ryans970@hotmail.com>
2017-08-09 15:23:57 -0400

    Optimized mover class

--- views/js/main.js
+++ views/js/main.js
@@ -493,11 +493,10 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
-  var scrollTop = document.body.scrollTop
+  var phase = Math.sin((document.body.scrollTop / 1250));
 
+  var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -526,6 +525,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    elem.style.willChange = "transform";
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
